90be52dedf3d6cc0012bbf1b8ba8bae455099598
Quoc Trung <quoctrung66@gmail.com>
2016-06-03 23:06:39 +0700

    Cài đặt Map -> Zoom đến HCMUT

--- app/src/main/java/com/github/quoctrung66/osmnavigation/MainActivity.java
+++ app/src/main/java/com/github/quoctrung66/osmnavigation/MainActivity.java
@@ -4,6 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.github.quoctrung66.osmnavigation.Handler.HandleView;
+import com.github.quoctrung66.osmnavigation.Helper.Constant;
+import com.github.quoctrung66.osmnavigation.View.MapViewCustom;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +20,21 @@ public class MainActivity extends AppCompatActivity {
 
         //Kiểm tra kết nối đến GPS và Network
         new HandleView(MainActivity.this).inicheck();
+
+        //Setting map
+        MapViewCustom mapView = (MapViewCustom) findViewById(R.id.map);
+        assert mapView != null;
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.setMultiTouchControls(true);
+        mapView.addTapListener(new MapViewCustom.OnTapListener() {
+            @Override
+            public void onMapTapped(GeoPoint geoPoint) {
+
+            }
+        });
+        //Map Controller
+        IMapController mapController = mapView.getController();
+        mapController.setZoom(17);
+        mapController.animateTo(Constant.HCMUT);
     }
 }
